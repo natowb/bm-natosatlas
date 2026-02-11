a088cd05dc4daaf7ed0d871c2dde95e34ab14bfa
Nato <44450112+natowb@users.noreply.github.com>
2026-02-12 00:11:21 +1300

    refactor: remove class files for screen wrappers

--- natosatlas-bta/src/main/java/dev/natowb/natosatlas/bta/client/BTAClientPlatform.java
+++ natosatlas-bta/src/main/java/dev/natowb/natosatlas/bta/client/BTAClientPlatform.java
@@ -2,10 +2,16 @@ package dev.natowb.natosatlas.bta.client;
 
 import dev.natowb.natosatlas.client.ClientPlatform;
 import dev.natowb.natosatlas.client.access.ScreenAccess;
+import dev.natowb.natosatlas.client.ui.UIScaleInfo;
 import dev.natowb.natosatlas.client.ui.elements.UIScreen;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.ScaledResolution;
+import net.minecraft.client.gui.Screen;
+import net.minecraft.client.util.debug.DebugRender;
 import net.minecraft.core.sound.SoundCategory;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class BTAClientPlatform extends ClientPlatform {
     public BTAClientPlatform() {
@@ -16,7 +22,72 @@ public class BTAClientPlatform extends ClientPlatform {
                 if (screen == null) {
                     mc.displayScreen(null);
                 } else {
-                    mc.displayScreen(new BTAScreenWrapper(screen));
+                    mc.displayScreen(new Screen() {
+                        @Override
+                        public void init() {
+                            super.init();
+                            screen.init(width, height);
+                            Keyboard.enableRepeatEvents(true);
+                        }
+
+                        @Override
+                        public void removed() {
+                            Keyboard.enableRepeatEvents(false);
+                            screen.onClose();
+                        }
+
+                        @Override
+                        public void tick() {
+                            screen.tick();
+                        }
+
+                        @Override
+                        public void render(int mouseX, int mouseY, float delta) {
+                            ScaledResolution ss = mc.resolution;
+                            UIScaleInfo info = new UIScaleInfo(ss.getScale(), ss.getScaledWidthScreenCoords(), ss.getScaledHeightScreenCoords());
+                            screen.render(mouseX, mouseY, delta, info);
+                        }
+
+                        @Override
+                        public void updateEvents() {
+                            int x = Mouse.getEventX() * width / mc.gameWindow.getWidthPixels();
+                            int y = height - Mouse.getEventY() * height / mc.gameWindow.getHeightPixels() - 1;
+                            while (Mouse.next()) {
+                                screen.handleRawMouseEvent(x, y, Mouse.getEventButton(), Mouse.getEventButtonState(), Mouse.getEventDWheel());
+                            }
+
+                            while (Keyboard.next()) {
+                                if (!Keyboard.getEventKeyState() || !(Boolean) this.mc.gameSettings.showFrameTimes.value || !DebugRender.keyPressed(Keyboard.getEventKey())) {
+                                    int eventKey = Keyboard.getEventKey();
+                                    char eventChar = Keyboard.getEventCharacter();
+                                    if (eventKey == 0 && Character.isDefined(eventChar)) {
+                                        this.keyPressed(eventChar, eventKey, x, y);
+                                        return;
+                                    }
+
+                                    if (Keyboard.getEventKeyState()) {
+                                        this.keyPressed(eventChar, eventKey, x, y);
+                                    }
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void keyPressed(char eventCharacter, int eventKey, int mx, int my) {
+                            screen.keyPressed(eventCharacter, eventKey);
+                        }
+
+
+                        @Override
+                        public void selectNextField() {
+                            screen.handleTab();
+                        }
+
+                        @Override
+                        public boolean isPauseScreen() {
+                            return false;
+                        }
+                    });
                 }
             }
 
--- natosatlas-modloader/src/main/java/dev/natowb/natosatlas/modloader/client/MLClientPlatform.java
+++ natosatlas-modloader/src/main/java/dev/natowb/natosatlas/modloader/client/MLClientPlatform.java
@@ -2,20 +2,74 @@ package dev.natowb.natosatlas.modloader.client;
 
 import dev.natowb.natosatlas.client.ClientPlatform;
 import dev.natowb.natosatlas.client.access.ScreenAccess;
+import dev.natowb.natosatlas.client.ui.UIScaleInfo;
 import dev.natowb.natosatlas.client.ui.elements.UIScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ScreenScaler;
 import net.minecraft.src.ModLoader;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class MLClientPlatform extends ClientPlatform {
     public MLClientPlatform() {
-        super(new MLPainter(),  new ScreenAccess() {
+        super(new MLPainter(), new ScreenAccess() {
             @Override
             public void openNacScreen(UIScreen screen) {
                 Minecraft mc = ModLoader.getMinecraftInstance();
                 if (screen == null) {
                     mc.setScreen(null);
                 } else {
-                    mc.setScreen(new MLScreenWrapper(screen));
+                    mc.setScreen(new Screen() {
+                        @Override
+                        public void init() {
+                            super.init();
+                            screen.init(width, height);
+                            Keyboard.enableRepeatEvents(true);
+                        }
+
+                        @Override
+                        public void removed() {
+                            Keyboard.enableRepeatEvents(false);
+                            screen.onClose();
+                        }
+
+                        @Override
+                        public void tick() {
+                            screen.tick();
+                        }
+
+                        @Override
+                        public void render(int mouseX, int mouseY, float delta) {
+                            ScreenScaler ss = new ScreenScaler(minecraft.options, minecraft.displayWidth, minecraft.displayHeight);
+                            screen.render(mouseX, mouseY, delta, new UIScaleInfo(ss.scaleFactor, ss.getScaledWidth(), ss.getScaledHeight()));
+                        }
+
+                        @Override
+                        public void onMouseEvent() {
+                            int x = Mouse.getEventX() * width / minecraft.displayWidth;
+                            int y = height - Mouse.getEventY() * height / minecraft.displayHeight - 1;
+                            screen.handleRawMouseEvent(x, y, Mouse.getEventButton(), Mouse.getEventButtonState(), Mouse.getEventDWheel());
+                        }
+
+                        @Override
+                        protected void keyPressed(char var1, int var2) {
+                            screen.keyPressed(var1, var2);
+                        }
+
+                        @Override
+                        protected void mouseClicked(int mouseX, int mouseY, int button) {
+                        }
+
+                        @Override
+                        protected void mouseReleased(int var1, int var2, int var3) {
+                        }
+
+                        @Override
+                        public boolean shouldPause() {
+                            return false;
+                        }
+                    });
                 }
             }
 
