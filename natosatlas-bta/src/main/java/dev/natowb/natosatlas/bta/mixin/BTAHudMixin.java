55b18516fee0bac13285c14c61b94f4f79477167
Nato <44450112+natowb@users.noreply.github.com>
2026-03-02 17:52:41 +1300

    fix: render minimap below debug info; dont render when f1 toggled

--- natosatlas-bta/src/main/java/dev/natowb/natosatlas/bta/mixin/BTAHudMixin.java
+++ natosatlas-bta/src/main/java/dev/natowb/natosatlas/bta/mixin/BTAHudMixin.java
@@ -3,20 +3,32 @@ package dev.natowb.natosatlas.bta.mixin;
 import dev.natowb.natosatlas.client.NAClient;
 import dev.natowb.natosatlas.client.ui.elements.UIScaleInfo;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScreenPhotoMode;
 import net.minecraft.client.gui.hud.HudIngame;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HudIngame.class)
 public class BTAHudMixin {
-    @Inject(method = "renderGameOverlay", at = @At("TAIL"))
+
+    @Inject(method = "renderGameOverlay", at = @At("HEAD"))
     private void natosatlas$drawMiniMap(float partialTicks, boolean flag, int mouseX, int mouseY, CallbackInfo ci) {
 
         Minecraft mc = Minecraft.getMinecraft();
         if (mc == null || mc.resolution == null) return;
+        if (mc.currentScreen instanceof ScreenPhotoMode) return;
+        if (!mc.gameSettings.immersiveMode.drawOverlays()) return;
+
+        // ----------------------------------------------
+        // copied straight from Hudingame.class
+        // ----------------------------------------------
+        mc.worldRenderer.setupScaledResolution();
+        GL11.glEnable(3042);
+        GL11.glBlendFunc(770, 771);
+        // ----------------------------------------------
 
         int scale = mc.resolution.getScale();
         int screenW = mc.resolution.getScaledWidthScreenCoords();
@@ -24,9 +36,9 @@ public class BTAHudMixin {
 
         UIScaleInfo scaleInfo = new UIScaleInfo(scale, screenW, screenH);
         NAClient client = NAClient.get();
+
         if (client != null) {
             client.renderGui(scaleInfo);
         }
-
     }
 }
--- natosatlas-stationapi/src/main/java/dev/natowb/natosatlas/stationapi/mixin/STHudMixin.java
+++ natosatlas-stationapi/src/main/java/dev/natowb/natosatlas/stationapi/mixin/STHudMixin.java
@@ -6,6 +6,7 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.ScreenScaler;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class STHudMixin {
-    @Inject(method = "render", at = @At("TAIL"))
+    @Inject(method = "render", at = @At("HEAD"))
     private void natosatlas$drawMiniMap(float screenOpen, boolean mouseX, int mouseY, int par4, CallbackInfo ci) {
         Minecraft mc = (Minecraft) FabricLoader.getInstance().getGameInstance();
         if (mc == null) return;
@@ -21,6 +22,9 @@ public class STHudMixin {
         int scale = scaler.scaleFactor;
         int screenW = scaler.getScaledWidth();
         int screenH = scaler.getScaledHeight();
+
+        mc.gameRenderer.setupHudRender();
+        GL11.glEnable(3042);
 
         UIScaleInfo scaleInfo = new UIScaleInfo(scale, screenW, screenH);
         NAClient client = NAClient.get();
